732dd16307c770c03a5332e73ec6d5b6d182a89f
DESKTOP-PE8APUQ\ScorpionIPX <scorpionipx@gmail.com>
2017-10-28 18:21:17 +0300

    Created project files

---
+++ AutoTrackingLDR/AutoTrackingLDR/main.c
@@ -0,0 +1,22 @@
+/*
+ * AutoTrackingLDR.c
+ *
+ * Created: 28-Oct-17 6:18:32 PM
+ * Author : ScorpionIPX
+ */ 
+
+#include "global.h"
+#include <avr/io.h>
+#include "adc_driver.h"
+#include "hx1230.h"
+#include "hx_8x6_characters.h"
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
